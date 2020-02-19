faaa3f6909aa18fe688fc75bde4dfa524a56b59d
aidandlim <aidandlim@gmail.com>
2020-02-19 13:50:58 -0800

    update modules

---
+++ client/build/precache-manifest.bc3ff422076862a0dc600b58d3c504b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8509de09c0bfd490cac1d7b3d2a6e525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63d0e6115d06c721cc7",
+    "url": "/static/css/main.61516c14.chunk.css"
+  },
+  {
+    "revision": "640529cb4be74c5fcbe7",
+    "url": "/static/js/2.096b22a5.chunk.js"
+  },
+  {
+    "revision": "b6890fa93d5a151b2cfa88dc6a8e464e",
+    "url": "/static/js/2.096b22a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63d0e6115d06c721cc7",
+    "url": "/static/js/main.926e9d66.chunk.js"
+  },
+  {
+    "revision": "01feecdd44eab8249b8a",
+    "url": "/static/js/runtime-main.da1601b7.js"
+  },
+  {
+    "revision": "e499f1fbaeb06cf2b9f6ddfd4de672b5",
+    "url": "/static/media/en_US.e499f1fb.png"
+  },
+  {
+    "revision": "54f03dc18423123e3c8d69a97b53bb0f",
+    "url": "/static/media/image_default.54f03dc1.png"
+  },
+  {
+    "revision": "b026a63067e5bf4be71d827a23411f07",
+    "url": "/static/media/ko_KR.b026a630.png"
+  },
+  {
+    "revision": "90529fa1a5f199491f84a8e2387c5383",
+    "url": "/static/media/user_default.90529fa1.png"
+  }
+]);
