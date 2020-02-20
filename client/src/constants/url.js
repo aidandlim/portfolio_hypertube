17eda3fe908b6e0523552cff63457507e50bc7dc
aidandlim <aidandlim@gmail.com>
2020-02-19 17:20:23 -0800

    update modules

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -2,10 +2,13 @@
 // const APP_PORT = '3000';
 // const SOCKET_PORT = '8445';
 
+// export const APP_URL = `${URL}:${APP_PORT}`;
+// export const APP_REDIRECT_URL = `${APP_URL}/auth/signin`;
+// export const SOCKET_URL = `${APP_URL}:${SOCKET_PORT}`;
+
 const URL = 'https://hypertube.aidandlim.com';
-const APP_PORT = '80';
 const SOCKET_PORT = '8445';
 
-export const APP_URL = `${URL}${APP_PORT === 80 ? null : ':' + APP_PORT}`;
+export const APP_URL = `${URL}`;
 export const APP_REDIRECT_URL = `${APP_URL}/auth/signin`;
 export const SOCKET_URL = `${APP_URL}:${SOCKET_PORT}`;
